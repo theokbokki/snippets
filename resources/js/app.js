15a46cf4deb8b3891ad6463a96eb7a414b61fccb
théoo <90846584+theokbokki@users.noreply.github.com>
2024-12-31 11:47:12 +0100

    Add metas, favicons and SM card

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,12 @@
 import Snippet from './parts/snippet';
+import.meta.glob([
+    '../img/**',
+    '../icons/**',
+    '../favicons/**.svg',
+    '../favicons/**.png',
+    '../favicons/**.ico',
+    '../favicons/**.webmanifest',
+]);
 
 class App {
     constructor() {
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,9 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snippets</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::asset('resources/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::asset('resources/favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ Vite::asset('resources/favicons/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ Vite::asset('resources/favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00a300">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Théo's snippet repository</title>
+
+    <meta name="keywords" content="Web, Code, Snippets, Go, PHP, Javascript, CSS, HTML, Blade, GLSL" />
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="snippets.theoo.dev">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@theokbokki_">
+    <meta name="twitter:creator" content="@theokbokki_">
+
+    <meta name="description" content="A collection of all my snippets with powerfull filtering capabilites." />
+    <meta property="og:url" content="{{ route('home') }}">
+    <meta property="og:title" content="Théo's snippet manager">
+    <meta property="og:image" content="{{ Vite::asset('resources/img/card.png') }}">
+    <meta property="og:description" content="A collection of all my snippets with powerfull filtering capabilites.">
+    <meta name="twitter:title" content="Snippets">
+    <meta name="twitter:description" content="A collection of all my snippets with powerfull filtering capabilites.">
+    <meta name="twitter:image:src" content="{{ Vite::asset('resources/img/card.png') }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="app">
